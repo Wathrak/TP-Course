993c1f8534303d73a7a129dc5cd93f7d2125a57f
Wathrak <wathrak1@gmail.com>
2024-10-23 16:11:45 +0700

    finished banner

--- e-commerce/src/App.vue
+++ e-commerce/src/App.vue
@@ -1,24 +1,34 @@
 <template>
   <div class="main">
-    <Categories title="Burger" amount="14" color="#F2FCE4" image="/src/assets/images/burger.png"/>
-    <Categories title="Peach" amount="17" color="#FFFCEB" image="/src/assets/images/peach.png"/>
-    <Categories title="Organic Kiwi" amount="21" color="#ECFFEC" image="/src/assets/images/kiwi.png"/>
-    <Categories title="Red Apple" amount="68" color="#FEEFEA" image="/src/assets/images/apple.png"/>
-    <Categories title="Snack" amount="34" color="#FFF3EB" image="/src/assets/images/snack.png"/>
-    <Categories title="Black Plum" amount="25" color="#FFF3FF" image="/src/assets/images/blueberry.png"/>
-    <Categories title="Vegetables" amount="65" color="#F2FCE4" image="/src/assets/images/cabbage.png"/>
-    <Categories title="Headphones" amount="33" color="#FFFCEB" image="/src/assets/images/headphone.png"/>
-    <Categories title="Cake & Milk" amount="54" color="#F2FCE4" image="/src/assets/images/snack2.png"/>
-    <Categories title="Orange" amount="63" color="#FFF3FF" image="/src/assets/images/orange.png"/>
+    <div class="label">
+      <Categories title="Burger" amount="14" color="#F2FCE4" image="/src/assets/images/burger.png"/>
+      <Categories title="Peach" amount="17" color="#FFFCEB" image="/src/assets/images/peach.png"/>
+      <Categories title="Organic Kiwi" amount="21" color="#ECFFEC" image="/src/assets/images/kiwi.png"/>
+      <Categories title="Red Apple" amount="68" color="#FEEFEA" image="/src/assets/images/apple.png"/>
+      <Categories title="Snack" amount="34" color="#FFF3EB" image="/src/assets/images/snack.png"/>
+      <Categories title="Black Plum" amount="25" color="#FFF3FF" image="/src/assets/images/blueberry.png"/>
+      <Categories title="Vegetables" amount="65" color="#F2FCE4" image="/src/assets/images/cabbage.png"/>
+      <Categories title="Headphones" amount="33" color="#FFFCEB" image="/src/assets/images/headphone.png"/>
+      <Categories title="Cake & Milk" amount="54" color="#F2FCE4" image="/src/assets/images/snack2.png"/>
+      <Categories title="Orange" amount="63" color="#FFF3FF" image="/src/assets/images/orange.png"/>
+    </div>
+    <br><br><br><br>
+
+    <div class="banner">
+      <Banner title="Everyday Fresh & Clean with Our Products" color="#F0E8D5" imgURL="/src/assets/images/onion.png"/>
+      <Banner title="Everyday Fresh & Clean with Our Products" color="#F3E8E8" imgURL="/src/assets/images/strawberry-juice.png"/>
+      <Banner title="Everyday Fresh & Clean with Our Products" color="#E7EAF3" imgURL="/src/assets/images/vegetable-basket.png"/>
+    </div>
   </div>
 </template>
 
 <script>
+import Banner from './components/Banner.vue';
 import Categories from './components/Categories.vue';
 
 export default {
     name: 'App',
-    components: { Categories },
+    components: { Categories, Banner },
     
     data() {
         return {
@@ -30,7 +40,14 @@ export default {
 
 <style>
   .main {
+    display: block;
+  }
+  .label {
     display: flex;
-    gap: 10px;
+    gap: 25px;
+  }
+  .banner {
+    display: flex;
+    gap: 25px;
   }
 </style>
---
+++ e-commerce/src/components/Banner.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="banner-layout" :style="{ backgroundColor: color }">
+        <div class="content-layout">
+            <div class="title"> {{ title }} </div>
+            <button> Shop Now &#x2192; </button>
+        </div>
+
+        <div class="img-layout">
+            <img :src="imgURL" alt="pic" />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['title', 'imgURL', 'color'],
+}
+</script>
+
+<style>
+.banner-layout {
+    width: 100%;
+    max-width: 512px;
+    height: 300px;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: row;
+    overflow: hidden;
+}
+
+.img-layout {
+    flex: 1; 
+    display: flex; 
+    justify-content: center;
+    align-items: center;
+}
+
+.img-layout img {
+    width: 100%;
+    height: 250px;
+    object-fit: contain;
+    align-self: flex-end; /* make it go bottom */
+}
+
+.content-layout {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 15px;
+    gap: 10px;
+    justify-content: center;
+}
+
+.title {
+    font-family: Quicksand, sans-serif;
+    font-size: 24px;
+    font-weight: 700;
+    line-height: 32px;
+    text-align: left;
+    color: #333;
+}
+
+button {
+    background-color: #3BB77E;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    padding: 8px 15px;
+    font-size: 16px;
+    font-weight: 500;
+    margin-top: 10px;
+}
+
+button:hover {
+    background-color: #32a46b;
+}
+</style>
